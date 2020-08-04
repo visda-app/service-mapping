82d14cf38221099def0c7b419172456358e01a70
Hossein Seyedmehdi <hosseins@surveymonkey.com>
2020-08-03 21:23:02 -0400

    Consumer consumes

--- src/consumer.py
+++ src/consumer.py
@@ -1,37 +1,30 @@
 import time
-import tensorflow_hub as hub
+import json
 
 
-from repo.schemas import TextSchema, TextEmbeddingSchema
+from repo.schemas import TextEmbeddingSchema
 from lib.utils import text_tip
 from lib.logger import logger
 from configs.app import (
     PulsarConf,
-    PATH_TO_NLP_MODEL
 )
 from message_broker import MessageBroker, Consumer, Producer
 
 
-logger.info("⌛ Loading model...")
-
-start_time = time.time()
-embed = hub.load(PATH_TO_NLP_MODEL)
-logger.info("✅ Model '{}' loaded in ⏱ {:.1f} secs.".format(
-    PATH_TO_NLP_MODEL, time.time() - start_time
-))
-
+logger.info("⌛ Connecting to the message broker...")
 mb = MessageBroker(
     broker_service_url=PulsarConf.client,
     consumer=Consumer(
-        PulsarConf.text_topic,
-        PulsarConf.subscription_name,
-        schema_class=TextSchema
-    ),
-    producer=Producer(
         PulsarConf.text_embedding_topic,
+        PulsarConf.subscription_name,
         schema_class=TextEmbeddingSchema
-    )
+    ),
 )
+logger.info("✅ Connection to the message broker established.")
+
+logger.info("Opening the output file...")
+FILE_NAME = "etc/output.json"
+ 
 
 logger.info("🔁 Starting the infinite loop ... ")
 while True:
@@ -40,19 +33,23 @@ while True:
     except KeyboardInterrupt:
         break
     try:
-        embedding = embed([msg.value().text])
+        [msg.value().text]
         logger.debug(
             f"uuid={msg.value().uuid}, "
             f"text='{text_tip(msg.value().text)}' "
-            "was vectorized! 🤓"
+            f"embedding='{msg.value().embedding}'"
+            "Received! 🤓"
         )
-        mb.producer_send(
-            TextEmbeddingSchema(
-                uuid=msg.value().uuid,
-                text=msg.value().text,
-                embedding=embedding.numpy().tolist()[0]
-            )
-        )
+        with open(FILE_NAME, "a") as f:
+            f.write(json.dumps(
+                {
+                    'uuid': msg.value().uuid,
+                    'text': msg.value().text,
+                    'embedding': msg.value().embedding
+                }
+            ))
+            f.write('\n')
+
         mb.consumer_acknowledge(msg)
     except Exception as e:
         # Message failed to be processed
